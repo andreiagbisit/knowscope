ca0ae2520e9871af7aa2171d90d46c76e8417070
andreiagbisit <alidrey912@gmail.com>
2025-11-21 03:55:49 +0800

    fixed user creation 2

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,11 +2,11 @@ import { Webhook } from 'svix'
 import User from '../models/User.js'
 
 // API CONTROLLER - MANAGE CLERK USER W/ DATABASE
-export const clerkWebhooks = async () => {
+export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(JSON.stringify(requestAnimationFrame.body), {
+        await whook.verify(JSON.stringify(req.body), {
             'svix-id': req.headers['svix-id'],
             'svix-timestamp': req.headers['svix-timestamp'],
             'svix-signature': req.headers['svix-signature']
