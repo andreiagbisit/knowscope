c1a9209a2f03b7573eed7f371bd46346370edfc4
andreiagbisit <alidrey912@gmail.com>
2025-11-23 01:11:22 +0800

    added stripe implementation

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,126 @@
+import { clerkClient } from '@clerk/express'
+import Course from '../models/Course.js'
+import { v2 as cloudinary } from 'cloudinary'
+import { Purchase } from '../models/Purchase.js'
+import User from '../models/User.js'
+
+// UPDATE ROLE TO EDUCATOR
+export const updateRoleToEducator = async(req, res) => {
+    try {
+        const { userId } = req.auth()
+        
+        await clerkClient.users.updateUserMetadata(userId, {
+            publicMetadata: {
+                role: 'educator',
+            }
+        })
+
+        res.json({ success: true, message: 'You may now publish a course.' })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// ADD NEW COURSE
+export const addCourse = async (req, res) => {
+    try {
+        const { courseData } = req.body
+        const imageFile = req.file
+        const educatorId = req.auth().userId
+
+        if(!imageFile) {
+            return res.json({ success: false, message: 'Thumbnail not attached.' })
+        }
+
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+        await newCourse.save()
+
+        res.json({ success: true, message: 'Course added.' })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// GET EDUCATOR COURSES
+export const getEducatorCourses = async (req, res) => {
+    try {
+        const educator = req.auth().userId
+        
+        const courses = await Course.find({educator})
+        res.json({ success: true, courses })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// GET EDUCATOR DASHBOARD DATA (TOTAL EARNING, ENROLLED STUDENTS, NO. OF COURSES )
+export const educatorDashboardData = async (req, res) => {
+    try {
+        const educator = req.auth().userId
+        const courses = await Course.find({educator})
+        const totalCourses = courses.length
+
+        const courseIds = courses.map(course => course._id)
+
+        // CALCULATE TOTAL EARNINGS FROM PURCHASES
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        })
+
+        const totalEarnings = purchases.reduce((sum, purchase) => sum + purchase.amount, 0)
+
+        // COLLECT UNIQUE ENROLLED STUDENT ID'S WITH THEIR COURSE TITLES
+        const enrolledStudentsData = []
+        for (const course of courses) {
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            }, 'name imageUrl')
+
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                })
+            })
+        }
+
+        res.json({ success: true, dashboardData: {
+            totalEarnings, enrolledStudentsData, totalCourses
+        }})
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// GET ENROLLED STUDENTS DATA W/ PURCHASE DATA
+export const getEnrolledStudentsData = async (req, res) => {
+    try {
+        const educator = req.auth().userId
+        const courses = await Course.find({educator})
+        const courseIds = courses.map(course => course._id)
+
+        const purchases = await Purchase.find({
+            courseId: { $in: courseIds },
+            status: 'completed'
+        }).populate('userId', 'name imageUrl').populate('courseId', 'courseTitle')
+
+        const enrolledStudents = purchases.map(purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseDate: purchase.createdAt
+        }))
+
+        res.json({success: true, enrolledStudents})
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,20 +2,31 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 // INITIALIZE EXPRESS
 const app = express()
 
 // CONNECT TO DATABASE
 await connectDB()
+await connectCloudinary()
 
 // MIDDLEWARE
 app.use(cors())
+app.use(clerkMiddleware())
 
 // ROUTES
 app.get('/', (req, res) => res.send('API working.'))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 // PORT
 const PORT = process.env.PORT || 5000
