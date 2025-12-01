f70ea117570cfc4cf66b32e58536575d3c572f5b
andreiagbisit <alidrey912@gmail.com>
2025-12-02 05:50:46 +0800

    reverted createRoot parent to BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,6 +13,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} 
                    afterSignOutUrl='/'
                    appearance={{
@@ -20,10 +21,9 @@ createRoot(document.getElementById('root')).render(
                       colorPrimary: '#00A63E',
                     },
                    }}>
-      <BrowserRouter>
-        <AppContextProvider>
-          <App />
-        </AppContextProvider>
-      </BrowserRouter>
-    </ClerkProvider>,
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </ClerkProvider>
+  </BrowserRouter>,
 )
