4bed6788d67bd62e8a231bb467dec417912cbc96
andreiagbisit <alidrey912@gmail.com>
2025-12-06 10:52:36 +0800

    replaced isLoaded with isLoading

--- client/src/components/educator/EducatorRoute.jsx
+++ client/src/components/educator/EducatorRoute.jsx
@@ -3,11 +3,11 @@ import { Navigate, useLocation } from 'react-router-dom'
 import Loading from '../student/Loading'
 
 function EducatorRoute ({ children }) {
-  const { isLoaded, isSignedIn, user } = useUser()
   const location = useLocation()
   const isEducatorPath = location.pathname.startsWith('/educator')
+  const { isLoading, isSignedIn, user } = useUser()
 
-  if (!isLoaded) return <Loading />
+  if (isLoading) return <Loading />
 
   if (!user || user.publicMetadata === undefined) {
     return <Loading />
