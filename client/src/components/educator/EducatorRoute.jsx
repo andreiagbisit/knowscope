e4689184b5c21fdebcacee04e74acb1f9367b019
andreiagbisit <alidrey912@gmail.com>
2025-12-06 10:17:53 +0800

    decluttered EducatorRoute 2

--- client/src/components/educator/EducatorRoute.jsx
+++ client/src/components/educator/EducatorRoute.jsx
@@ -5,22 +5,15 @@ import Loading from '../student/Loading'
 function EducatorRoute ({ children }) {
   const { isLoaded, isSignedIn, user } = useUser()
   const location = useLocation()
-
   const isEducatorPath = location.pathname.startsWith('/educator')
 
   if (!isLoaded) return <Loading />
-
-  if (!isSignedIn) {
-    return (
-      <Navigate to={isEducatorPath ? '/' : location.pathname} replace />
-    )
-  }
 
   if (!user || user.publicMetadata === undefined) {
     return <Loading />
   }
 
-  if (user.publicMetadata.role !== 'educator') {
+  if (!isSignedIn || user.publicMetadata.role !== 'educator') {
     return (
       <Navigate to={isEducatorPath ? '/' : location.pathname} replace />
     )
